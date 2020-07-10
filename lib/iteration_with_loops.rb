afd8011313327ea0d4f5a9dd810a7ac39f54e9b3
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-10 19:25:44 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,21 +5,6 @@ src.each do |i|
   outer_results << i.min
 end
 end
-while row_index < src.count do
-  element_index = 0
-  while element_index < src[row_index].count do
-    # How to read the following line of code:
-    #   Array at row_index
-    #   Element of the inner array at element_index
-    #   The first character of that element...
-    if src[row_index][element_index] <= src[row_index][] do
-      outer_results << src[row_index][element_index]
-    end
-    end
-    element_index += 1
-  end
-  row_index += 1
-end
 outer_results = src
 end
 
